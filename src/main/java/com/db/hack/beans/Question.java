f241f603a6df71c4533e7df81bfe95513cabc792
Roshan K <roshan.arians@gmail.com>
2019-06-04 15:35:50 -0400

    Adding question model

---
+++ src/main/java/com/db/hack/beans/Question.java
@@ -0,0 +1,89 @@
+package com.db.hack.beans;
+
+/**
+ * Created by Roshan on 6/4/2019.
+ */
+public class Question {
+
+    private String questionId;
+    private String question;
+    private String optionOne;
+    private String optionTwo;
+    private String optionThree;
+    private String optionFour;
+    private String correctAnswer;
+    private String topic;
+    private String grade;
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOptionOne() {
+        return optionOne;
+    }
+
+    public void setOptionOne(String optionOne) {
+        this.optionOne = optionOne;
+    }
+
+    public String getOptionTwo() {
+        return optionTwo;
+    }
+
+    public void setOptionTwo(String optionTwo) {
+        this.optionTwo = optionTwo;
+    }
+
+    public String getOptionThree() {
+        return optionThree;
+    }
+
+    public void setOptionThree(String optionThree) {
+        this.optionThree = optionThree;
+    }
+
+    public String getOptionFour() {
+        return optionFour;
+    }
+
+    public void setOptionFour(String optionFour) {
+        this.optionFour = optionFour;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
