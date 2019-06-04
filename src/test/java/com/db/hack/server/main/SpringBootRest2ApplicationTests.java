31bf7d4ddbdac22cf4f357e8f1b35eb4f0b0e982
Shirish Sahu <shirish2005@gmail.com>
2019-06-04 15:35:44 -0400

    format

--- src/test/java/com/db/hack/server/main/SpringBootRest2ApplicationTests.java
+++ src/test/java/com/db/hack/server/main/SpringBootRest2ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.db.hack.server.main;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
