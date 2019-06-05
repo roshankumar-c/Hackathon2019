ecce1e789b2f273b42644072145fa90da345ab9b
Roshan K <roshan.arians@gmail.com>
2019-06-05 16:35:44 -0400

    updated from int to string

--- src/main/java/com/db/hack/data/save/QuizResultSave.java
+++ src/main/java/com/db/hack/data/save/QuizResultSave.java
@@ -4,9 +4,7 @@ import com.db.hack.beans.Question;
 import com.db.hack.beans.QuizResult;
 import com.db.hack.databse.DatabaseConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,17 +25,34 @@ public class QuizResultSave {
         Connection conn = dataBaseConnectionFactory.getConnection();
         if (conn!=null)
         {
-
             try
             {
-                Statement select = conn.createStatement();
-                select.execute("INSERT INTO dbo.test_results (user_id, quiz_id, score) VALUES("+quizResult.getUserid()+","+quizResult.getQuizid()+","+quizResult.getTestscore()+")");
-                System.out.println("saved data in databse");
+                // the mysql insert statement
+                String query = "INSERT INTO dbo.test_results (user_id, quiz_id, score) VALUES"
+                        + " (?, ?, ?)";
+
+                // create the mysql insert preparedstatement
+                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                preparedStmt.setString (1, quizResult.getUserid());
+                preparedStmt.setInt (2, Integer.parseInt(quizResult.getQuizid()));
+                preparedStmt.setInt   (3, Integer.parseInt(quizResult.getTestscore()));
+
+                // execute the preparedstatement
+                preparedStmt.execute();
+               // Statement select = conn.createStatement();
+                //select.execute("INSERT INTO dbo.test_results (user_id, quiz_id, score) VALUES("++","+quizResult.getQuizid()+","+quizResult.getTestscore()+")");
+                System.out.println("saved data in database");
                 return true;
             }
             catch(Exception e)
             {
                 e.printStackTrace();
+            }finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
